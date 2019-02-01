530132a1fe6c1694159b6284b2b304cf6dfc2d06
esote <25505632+esote@users.noreply.github.com>
2019-01-31 22:51:16 -0600

    Move floor init to arrange_floor, warn upon resize

--- assignment-1.02/opal.c
+++ assignment-1.02/opal.c
@@ -10,30 +10,15 @@
 #define STAIR_UPCOUNT	rrand(1, (ROOMCOUNT / 4) + 1)
 #define STAIR_DOWNCOUNT	rrand(1, (ROOMCOUNT / 4) + 1)
 
-int
-main(int argc, char *argv[])
+static void
+arrange_floor(WINDOW *const win, int const w, int const h,
+	struct room *const rooms)
 {
-	WINDOW *win;
-	int h, w, ch;
-	unsigned int seed;
-	struct room rooms[ROOMCOUNT];
 	size_t i, retries;
-
-	seed = init_rand(argc >= 2 ? argv[1] : NULL);
-
-	win = initscr();
-	box(win, 0, 0);
-	getmaxyx(win, h, w);
-	noecho();
-	raw();
-
-	mvprintw(h-2, 2, "Press 'q' to quit.");
 
 	for (i = 0; i < ROOMCOUNT; ++i) {
 		gen_room(&rooms[i], w, h);
 	}
-
-	retries = 0;
 
 	for (i = 0; i < ROOMCOUNT && retries < ROOMRETRIES; ++i) {
 		if (!draw_room(win, rooms[i])) {
@@ -53,12 +38,32 @@ main(int argc, char *argv[])
 	for (i = 0; i < (size_t)STAIR_DOWNCOUNT; ++i) {
 		draw_stair(win, w, h, false);
 	}
+}
 
-	wrefresh(win);
+int
+main(int argc, char *argv[])
+{
+	WINDOW *win;
+	struct room rooms[ROOMCOUNT];
+	int h, w, ch;
+	unsigned int const seed = init_rand(argc >= 2 ? argv[1] : NULL);
 
-	while ((ch = getch()) != 'q');
+	win = initscr();
+	box(win, 0, 0);
+	getmaxyx(win, h, w);
+	noecho();
+	raw();
 
-	clear();
+	mvprintw(h-2, 2, "Press 'q' to quit.");
+
+	arrange_floor(win, w, h, rooms);
+
+	while ((ch = getch()) != 'q') {
+		if (ch == KEY_RESIZE) {
+			mvprintw(1, 1, "Resizing screen is undefined behavior");
+		}
+	}
+
 	endwin();
 
 	printf("seed: %u\n", seed);
